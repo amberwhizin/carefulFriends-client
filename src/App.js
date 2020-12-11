24ada3c86624d9e026ed9f96976c1ee0893e689e
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-11 11:31:37 -0500

    no more CORS errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  //const [hasErrors, setErrors] = useState(false);
+  const [activity, setActivity] = useState([]);
+
+  useEffect(() => {
+    fetch("/activities", {
+      method: "POST",
+      body: JSON.stringify({
+        activity,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        console.log({ res });
+        return res.json();
+      })
+      .then((data) => {
+        console.log({ data });
+        setActivity({ data });
+      })
+      .catch((data) => {
+        setActivity(data);
+      });
+
+    // get
+    fetch("/activities", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((parsedData) => setActivity({ activity: parsedData }));
+  }, []);
+
   return (
     <div className="container-fullwidth">
-      <h1>hi</h1>
+      <h1>Current Activity {JSON.stringify(activity)}</h1>
     </div>
   );
-}
+};
 
 export default App;
