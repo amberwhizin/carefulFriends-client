2a90f87ce9397d6bdb726e5eeea4408275c19661
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-14 20:49:32 -0500

    tidied code

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
 import Options from "./Options";
