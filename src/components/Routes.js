dcdf49360d10fab26fc2071922d3534f4a88bf05
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-15 14:57:08 -0500

    login route

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
 import Options from "./Options";
+import Login from "./Login";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/options" component={Options} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   );
 };
