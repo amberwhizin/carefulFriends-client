619479634ef34402951503fc83496f2408b1e1d7
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-16 19:24:48 -0500

    WIP: commenting on activities

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,63 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 import { Link, useHistory } from "react-router-dom";
+import { FaRegCommentDots } from "react-icons/fa";
+
+const ActivityCard = ({ item }) => {
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [comment, setComment] = useState("");
+  const createComment = (e) => {
+    e.preventDefault();
+    setComment("");
+    setIsCommenting(false);
+    console.log(comment);
+  };
+  return (
+    <div key={item._id} className="posted-activity">
+      <Card
+        className="shadow-sm p-3 mb-5 bg-white rounded"
+        style={{
+          width: "20rem",
+        }}
+      >
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <h4>{item.owner} did</h4>
+            <h4>{item.activityName}!</h4>
+          </ListGroup.Item>
+          {item.comments.map((activitiesComment) => {
+            return (
+              <ListGroup.Item key={activitiesComment._id}>
+                {activitiesComment.text}
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+        {!isCommenting && (
+          <Button onClick={() => setIsCommenting(true)}>
+            <FaRegCommentDots />
+          </Button>
+        )}
+        {isCommenting && (
+          <Form onSubmit={createComment}>
+            <InputGroup className="mb-2 mr-sm-2">
+              <FormControl
+                placeholder="Write comment"
+                onChange={(e) => setComment(e.target.value)}
+              />
+            </InputGroup>
+          </Form>
+        )}
+      </Card>
+    </div>
+  );
+};
 
 const Home = () => {
   const [activitiesList, setActivitiesList] = useState([]);
@@ -27,7 +83,7 @@ const Home = () => {
   };
 
   return (
-    <div className="container">
+    <Container>
       {/* message board */}
       <h1 className="title">Community Encouragements</h1>
       <Link to="/activities">
@@ -36,22 +92,9 @@ const Home = () => {
         </Button>
       </Link>
       {activitiesList.map((item) => {
-        console.log(item);
-        return (
-          <div key={item._id} className="posted-activity">
-            <Card
-              className="shadow-sm p-3 mb-5 bg-white rounded"
-              style={{ width: "20rem" }}
-            >
-              <Card.Body>
-                <h4>{item.owner} did</h4>
-                <h4>{item.activityName}!</h4>
-              </Card.Body>
-            </Card>
-          </div>
-        );
+        return <ActivityCard item={item} key={item._id} />;
       })}
-    </div>
+    </Container>
   );
 };
 
