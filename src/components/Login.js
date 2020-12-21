6db4113dacf0ab86a217c38a4e8f5398118b2782
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-20 20:58:49 -0500

    Fix is back

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     axios
       .post(
-        "https://carefulfriends-api.herokuapp.com/activities",
+        "https://carefulfriends-api.herokuapp.com/login",
         { name: user, password },
         {
           withCredentials: true,
